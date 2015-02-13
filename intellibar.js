8718fccf172f03f0d62c4e975043009273775e6c
Mikael Engver <mikael.engver@gmail.com>
2015-02-13 10:26:34 +0100

    Moved Demography to be the first tab.

--- intellibar.js
+++ intellibar.js
@@ -18,8 +18,8 @@ window.intellibar = (function () {
               '<div id="barmenu-wrapper">'+
                 '<img src="'+BASE_URL+'/tv4play.png" id="logo" />'+
                 '<ul id="barmenu">'+
-                  '<li id="reach_visits" data-type="iframe">Räckvidd och besök</li>'+
                   '<li id="demography" data-type="doubleIframe">Demografi</li>'+
+                  '<li id="reach_visits" data-type="iframe">Räckvidd och besök</li>'+
                   '<li id="social_media" data-type="doubleIframe">Sociala Medier</li>'+
                   '<li id="day_time" data-type="iframe">Dag &amp; tid</li>'+
                   '</ul>'+
@@ -30,12 +30,11 @@ window.intellibar = (function () {
   var setBar = function(id, type) {
 
     var contentSrc = {
-      reach_visits: 'https://docs.google.com/a/burtcorp.com/spreadsheets/d/1r46AeAImleMM7jKS2t8SGtZXwhMRBrB5VpgoBr8gNVc/pubchart?oid=1843346657&format=interactive',
       demography : {
                     first: 'https://docs.google.com/a/burtcorp.com/spreadsheets/d/1r46AeAImleMM7jKS2t8SGtZXwhMRBrB5VpgoBr8gNVc/pubchart?oid=1631858610&format=interactive',
                     second: 'https://docs.google.com/a/burtcorp.com/spreadsheets/d/1r46AeAImleMM7jKS2t8SGtZXwhMRBrB5VpgoBr8gNVc/pubchart?oid=1231345853&format=interactive'
-                    // second: 'https://docs.google.com/a/burtcorp.com/spreadsheets/d/11ZPH4egIoyLsxhpgktz-LpXsjA2-1ihTvNT7i0zk1WE/pubchart?oid=636087322&format=interactive'
                   },
+      reach_visits: 'https://docs.google.com/a/burtcorp.com/spreadsheets/d/1r46AeAImleMM7jKS2t8SGtZXwhMRBrB5VpgoBr8gNVc/pubchart?oid=1843346657&format=interactive',
       social_media : {
                     first: 'https://docs.google.com/a/burtcorp.com/spreadsheets/d/1tisxpbbyciDxifWw1QAtki0-uSMfKRvlXhw6uoUTahc/pubchart?oid=1177234689&format=interactive',
                     second: 'https://docs.google.com/spreadsheets/d/1tisxpbbyciDxifWw1QAtki0-uSMfKRvlXhw6uoUTahc/pubchart?oid=1346713540&format=interactive'
@@ -74,7 +73,7 @@ window.intellibar = (function () {
   });
 
   $('body').css({'padding-bottom':225}).append($bar).scroll
-  setBar('reach_visits', 'iframe');
+  setBar('demography', 'iframe');
   console.log('### TV4 IntelliBar loaded! ###')
 
 })();
